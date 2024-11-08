ad6574fe27d0b5cbeeb86e32baf3ebc15e7f9a81
Imran Farooq <imran_farooq7@hotmail.com>
2024-11-09 03:44:57 +0500

    App completed

---
+++ components/CreditUsageBar.tsx
@@ -0,0 +1,38 @@
+import { countContext } from "@/context/UsageProvider";
+import { useContext } from "react";
+
+const CreditsUsageBar = () => {
+	const ctx = useContext(countContext);
+	const credits = 10000;
+	const percentage =
+		ctx?.subStatus === "active" ? 100 : (ctx?.count! / credits) * 100;
+	return (
+		<div>
+			<p className="font-bold text-white">Credits usage</p>
+			<div className="mt-3" aria-hidden="true">
+				<div className="overflow-hidden rounded-full bg-gray-200">
+					<div
+						className="h-2 rounded-full bg-indigo-600"
+						style={{
+							width: `${percentage}%`,
+						}}
+					/>
+				</div>
+				<div className="mt-3 text-white font-semibold">
+					<div>
+						{ctx?.subStatus === "active"
+							? "unlimit credits"
+							: `${ctx?.count! / credits} usage`}
+					</div>
+				</div>
+				{ctx?.subStatus === "active" ? null : (
+					<button className="hover:scale-105 transition-all ease-in-out bg-emerald-500 my-4 text-white w-full px-4 py-3 rounded-lg">
+						Upgrade
+					</button>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default CreditsUsageBar;
